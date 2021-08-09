312bf6131ba0498314b11d043f596b342940e020
RADAJO2 <radajo2@oocl.com>
2021-08-09 21:50:14 +0800

    [WordFrequency][Joanna] refactor: remove unnecessary code

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -29,26 +29,6 @@ public class WordFrequencyGame {
         }
     }
 
-    private List<WordInfo> calculateWordFrequency(String sentence) {
-        String[] words = sentence.split(BLANK_SPACE);
-
-        List<WordInfo> wordInfoList = new ArrayList<>();
-        for (String word : words) {
-            WordInfo wordInfo = new WordInfo(word, 1);
-            wordInfoList.add(wordInfo);
-        }
-        //get the map for the next step of sizing the same word
-        Map<String, List<WordInfo>> map =getListMap(wordInfoList);
-
-        List<WordInfo> list = new ArrayList<>();
-        for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()){
-            WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
-            list.add(wordInfo);
-        }
-        wordInfoList = list;
-        return wordInfoList;
-    }
-
     private List<WordInfo> calculateWordFrequencyTemp(String sentence) {
         List<String> words = Arrays.asList(sentence.split(BLANK_SPACE));
         List<String> distinctWords = words.stream().distinct().collect(Collectors.toList());
@@ -62,20 +42,4 @@ public class WordFrequencyGame {
         return wordInfos;
     }
 
-    private Map<String,List<WordInfo>> getListMap(List<WordInfo> wordInfoList) {
-        Map<String, List<WordInfo>> map = new HashMap<>();
-        for (WordInfo wordInfo : wordInfoList){
-//       map.computeIfAbsent(wordInfo.getValue(), k -> new ArrayList<>()).add(wordInfo);
-            if (!map.containsKey(wordInfo.getValue())){
-                ArrayList arr = new ArrayList<>();
-                arr.add(wordInfo);
-                map.put(wordInfo.getValue(), arr);
-            }
-
-            else {
-                map.get(wordInfo.getValue()).add(wordInfo);
-            }
-        }
-        return map;
-    }
 }
